c3089a8f2aee06f573d9deba5a81993b8d299686
jk960-max <jk960@cairn.edu>
2026-04-28 19:34:34 -0400

    Update parser.js

--- parser.js
+++ parser.js
@@ -1,5 +1,46 @@
 function parse(tokens) {
   let i = 0;
 
-  function parseStatements 
+  function parseStatement() {
+    let token = tokens[i];
+
+    if (token.type === "IDENTIFIER"){
+      let name = token.value;
+      i++; // move past identifier
+
+      if (tokens[i].type !==  "EQUALS"){
+        throw new Error ("Expected =");
+      } 
+
+      i++; // skip '='
+
+      let value = tokens[i].value;
+      i++;
+
+      return {
+        type: "Assignment"
+        name,
+        value
+      };
+  }
+  
+  if (token.type === "PRINT") {
+    i++;
+    let value = tokens[i].value;
+    i++;
+
+    return {
+      type: "Print",
+      value
+    };
+  }
+}  
+
+const body = [];
+while (i < tokens.length) {
+  body.push(parseStatement());
 }
+return { type: "Program", body };
+}
+
+module.exports = { parse };
