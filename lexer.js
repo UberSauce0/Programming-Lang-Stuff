ea5691d0317e5a5d75c4194d0f6805fed47bf684
UberSauce0 <ryanzett@gmail.com>
2026-04-28 19:22:46 -0400

    Update lexer.js

--- lexer.js
+++ lexer.js
@@ -1,1 +1,45 @@
+const grammar = {
+  variable_declaration: [
+  "KEYWORD(let)",
+  "IDENTIFIER",
+  "EQUALS",
+  "VALUE",
+  "SEMICOLON",
+  ],
+  
+  play: [
+  "KEYWORD(play)",
+  "LPAREN",
+  "IDENTIFIER",
+  "RPAREN",
+  "SEMICOLON",
+  ],
+  
+  passPlay: [
+  "KEYWORD(passPlay)",
+  "LPAREN",
+  "STRING",
+  "RPAREN",
+  "SEMICOLON",
+  ],
+  
+  drive [
+  "KEYWORD(drive)",
+  "LPAREN",
+  "CONDITION",
+  "RPAREN",
+  "SEMICOLON",
+  ],
+  
+  function: [
+  "KEYWORD(passPlay)",
+  "LPAREN",
+  "ARG",
+  "RPAREN",
+  "SEMICOLON",
+  ],
+};
 
+const tokens = [
+  {}
+]
